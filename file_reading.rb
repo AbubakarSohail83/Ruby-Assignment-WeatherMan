bce69e73d5700b6fd1812dda4c715b919ed39d87
Abubakar-Sohail <abubakar.sohail@devsinc.com>
2023-08-10 13:13:31 +0500

    Optimized the code a bit more, and finalized it

--- file_reading.rb
+++ file_reading.rb
@@ -3,22 +3,16 @@
 module FileReading
   def read_files_from_folder(path_of_folder, date)
     file_names_array = Dir.glob("#{path_of_folder}/*.txt").select { |filename| filename.include?(date.to_s) }
-    files_array = []
+    data_array = []
     file_names_array.each do |file_name|
-      individual_file_content = []
-      File.open(file_name, 'r') do |file|
-        file.each_line do |file_line|
-          individual_file_content.push(file_line) if valid_line?(file_line.chomp)
-        end
-      end
-      files_array.push(individual_file_content)
+      data_array.concat File.readlines(file_name)
     end
-    files_array
+    data_array.delete_if { |line| !valid_line?(line) }
   rescue StandardError => e
     e.backtrace_locations
   end
 
   def valid_line?(line)
-    !(line.include?('<!-- 0.210:0 -->') || line.include?('Mean TemperatureC') || line.split(',')[1].nil?)
+    !line.include?('<!--') && !line.include?('Mean TemperatureC') && !line.chomp.empty?
   end
 end
--- monthly_weather_metrics.rb
+++ monthly_weather_metrics.rb
@@ -4,9 +4,12 @@ require 'colorize'
 require_relative 'file_reading'
 class MonthlyWeatherMetrics
   include FileReading
-  attr_accessor :avg_highest_temp, :avg_lowest_temp, :avg_humidity
+  attr_accessor :avg_max_temp, :avg_min_temp, :avg_humidity
 
   def initialize(date, path)
+    @avg_max_temp = { sum: 0.0, count: 0.0 }
+    @avg_min_temp = { sum: 0.0, count: 0.0 }
+    @avg_humidity = { sum: 0.0, count: 0.0 }
     months = {
       1 => 'Jan',
       2 => 'Feb',
@@ -29,53 +32,39 @@ class MonthlyWeatherMetrics
   end
 
   def calc_avg_metrics
-    temp_data = [
-      { sum: 0.0,  index: 1, result: 0.0 },
-      { sum: 0.0,  index: 3, result: 0.0 },
-      { sum: 0.0,  index: 7, result: 0.0 }
-    ]
-    count = 0.0
-    @file_data.each do |file|
-      file.each do |line|
-        temp_data.each do |temp|
-          temp[:sum] += line.chomp.split(',')[temp[:index]].to_f
-        end
-        count += 1
-      end
+    @file_data.each do |line|
+      @avg_max_temp[:sum] += line.split(',')[1].to_f unless line.split(',')[1].nil?
+      @avg_min_temp[:sum] += line.split(',')[3].to_f unless line.split(',')[3].nil?
+      @avg_humidity[:sum] += line.split(',')[7].to_f unless line.split(',')[7].nil?
+      @avg_max_temp[:count] += 1 unless line.split(',')[1].nil?
+      @avg_min_temp[:count] += 1 unless line.split(',')[3].nil?
+      @avg_humidity[:count] += 1 unless line.split(',')[7].nil?
     end
-    temp_data.each do |temp|
-      temp[:result] = (temp[:sum] / count).round(2)
-    end
-    @avg_highest_temp = temp_data[0][:result]
-    @avg_lowest_temp = temp_data[1][:result]
-    @avg_humidity = temp_data[2][:result]
+    @avg_max_temp[:result] = (@avg_max_temp[:sum] / @avg_max_temp[:count]).round(2)
+    @avg_min_temp[:result] = (@avg_min_temp[:sum] / @avg_min_temp[:count]).round(2)
+    @avg_humidity[:result] = (@avg_humidity[:sum] / @avg_humidity[:count]).round(2)
   rescue StandardError => e
     p e.backtrace_locations
-    p e.message
   end
-
-
 
   def draw_monthly_weather_chart
     puts "#{@month} #{@year}"
     count = 1
-    @file_data.each do |file|
-      file.each do |line|
-        line_parts = line.chomp.split(',')
-        daily_high_temp = line_parts[1].to_i
-        daily_low_temp = line_parts[3].to_i
-        print count
-        daily_high_temp.times do
-          print '+'.colorize(:red)
-        end
-        print " #{daily_high_temp}C \n"
-        print count
-        daily_low_temp.times do
-          print '+'.colorize(:blue)
-        end
-        print " #{daily_low_temp}C \n"
-        count += 1
+    @file_data.each do |line|
+      line_parts = line.chomp.split(',')
+      daily_high_temp = line_parts[1].to_i
+      daily_low_temp = line_parts[3].to_i
+      print count
+      daily_high_temp.times do
+        print '+'.colorize(:red)
       end
+      print " #{daily_high_temp}C \n"
+      print count
+      daily_low_temp.times do
+        print '+'.colorize(:blue)
+      end
+      print " #{daily_low_temp}C \n"
+      count += 1
     end
   rescue StandardError => e
     e.backtrace_locations
